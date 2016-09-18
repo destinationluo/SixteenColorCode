d044d3bc32c79147623a755368ed457893452316
Luo Qian <luoqian0iris@gmail.com>
2016-09-18 10:54:36 +0800

    颜色

---
+++ SixteenColorCode/SixteenColorCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SixteenColorCode
+//
+//  Created by 罗倩 on 16/9/18.
+//  Copyright © 2016年 罗倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SixteenColorCode/SixteenColorCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SixteenColorCode
+//
+//  Created by 罗倩 on 16/9/18.
+//  Copyright © 2016年 罗倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
